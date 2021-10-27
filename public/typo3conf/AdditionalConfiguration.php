9280b86564c39cc2f5e4f891b9ac91d252f354d8
christi4n <christian@mydevfactory.net>
2021-10-27 22:52:25 +0200

    feat: Update additionalConfiguration

--- public/typo3conf/AdditionalConfiguration.php
+++ public/typo3conf/AdditionalConfiguration.php
@@ -28,6 +28,7 @@ $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = getenv('TYPO3_MAI
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_username'] = getenv('TYPO3_MAIL_TRANSPORT_SMTP_USERNAME');
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['defaultMailFromAddress'] = getenv('TYPO3_MAIL_DEFAULT_FROM');
 
+# Application contexts
 $appContext = (string)\TYPO3\CMS\Core\Core\Environment::getContext();
 switch ($appContext) {
 
@@ -48,39 +49,4 @@ switch ($appContext) {
     case 'Production/Validation':
     case 'Production':
         break;
-}
-
-if (getenv('IS_DDEV_PROJECT') == 'true') {
-    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
-        $GLOBALS['TYPO3_CONF_VARS'],
-        [
-            'DB' => [
-                'Connections' => [
-                    'Default' => [
-                        'dbname' => 'db',
-                        'host' => 'ddev-typo3v11-db',
-                        'password' => 'db',
-                        'port' => '3306',
-                        'user' => 'db',
-                    ],
-                ],
-            ],
-            // This GFX configuration allows processing by installed ImageMagick 6
-            'GFX' => [
-                'processor' => 'ImageMagick',
-                'processor_path' => '/usr/bin/',
-                'processor_path_lzw' => '/usr/bin/',
-            ],
-            // This mail configuration sends all emails to mailhog
-            'MAIL' => [
-                'transport' => 'smtp',
-                'transport_smtp_server' => 'localhost:1025',
-            ],
-            'SYS' => [
-                'trustedHostsPattern' => '.*.*',
-                'devIPmask' => '*',
-                'displayErrors' => 1,
-            ],
-        ]
-    );
 }
